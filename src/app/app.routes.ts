aa3bbbfff101e6c8acbf05130a599880fad7befd
Nicolás Doyhenart <nidoyhe@gmail.com>
2026-05-19 19:48:59 -0300

    Update guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
+import { publicGuard } from './guards/public.guard';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'registro', component: RegistroComponent },
+    { path: 'login', component: LoginComponent, canActivate: [publicGuard] },
+    { path: 'registro', component: RegistroComponent, canActivate: [publicGuard] },
     { path: 'quien-soy', component: QuienSoyComponent },
     // Ruta por defecto que redirige al Home
     { path: '', redirectTo: '/', pathMatch: 'full' },
